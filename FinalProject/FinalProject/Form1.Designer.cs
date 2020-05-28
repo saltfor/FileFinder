c5bdf5567b8616c310163e29d99552dc5d7965c6
Mahmut Arslan <34754922+saltfor@users.noreply.github.com>
2020-05-29 00:23:06 +0300

    Initial commit

---
+++ FinalProject/FinalProject/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace FinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose Directory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Gray;
+            this.listBox1.ForeColor = System.Drawing.Color.DarkRed;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(48, 178);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(181, 147);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Chose Directory";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "File Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(232, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "File Size";
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.Color.Gray;
+            this.listBox2.ForeColor = System.Drawing.Color.DarkRed;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(235, 178);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(91, 147);
+            this.listBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(329, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "File Location";
+            // 
+            // listBox3
+            // 
+            this.listBox3.BackColor = System.Drawing.Color.Gray;
+            this.listBox3.ForeColor = System.Drawing.Color.DarkRed;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(332, 178);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(312, 147);
+            this.listBox3.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(284, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 47);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Start for Searching";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(509, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 47);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Save Results to XML";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 356);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ FinalProject/FinalProject/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace FinalProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button2.Enabled = false;
+            button3.Enabled = false;
+        }
+
+        string directory = "";
+        int size = 0;
+        ArrayList files = new ArrayList();
+        List<long> fileSize = new List<long>();
+        ArrayList fileDirectory = new ArrayList();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog directchoosedlg = new FolderBrowserDialog();
+            if (directchoosedlg.ShowDialog() == DialogResult.OK)
+            {
+                directory = directchoosedlg.SelectedPath;
+                label1.Text = directory;
+                button1.Enabled = false;
+                DirSearch(directory);
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            for(int i = 0; i < size; i++)
+            {
+                bool isSame = false;
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (files[i].ToString() == files[j].ToString() && fileSize[i]==fileSize[j] && files[j].ToString()!="-")
+                    {
+                        isSame = true;
+                        listBox1.Items.Add(files[j]);
+                        listBox2.Items.Add(fileSize[j] + " KB");
+                        listBox3.Items.Add(fileDirectory[j]);
+                        files[j] = "-";
+                    }
+                }
+                if (isSame)
+                {
+                    listBox1.Items.Add(files[i]);
+                    listBox2.Items.Add(fileSize[i] + " KB");
+                    listBox3.Items.Add(fileDirectory[i]);
+                }
+            }
+
+            button3.Enabled = true;
+        }
+        void DirSearch(string sDir)
+        {
+            try
+            {
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    foreach (string f in Directory.GetFiles(d, "*.*"))
+                    {
+                        FileInfo info = new FileInfo(f);
+                        size++;
+                        files.Add(info.Name);
+                        fileSize.Add(info.Length);
+                        fileDirectory.Add(info.DirectoryName);
+                    }
+                    DirSearch(d);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox2.SelectedIndex = listBox1.SelectedIndex;
+            listBox3.SelectedIndex = listBox1.SelectedIndex;
+        }
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml("<files></files>");
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                XmlElement newElem = doc.CreateElement("file");
+                newElem.InnerText = listBox1.Items[i] + " - " + listBox2.Items[i] + " - " + listBox3.Items[i];
+                doc.DocumentElement.AppendChild(newElem);
+            }
+
+            doc.PreserveWhitespace = true;
+            doc.Save("data.xml");
+            MessageBox.Show("Data has saved to XML!");
+        }
+    }
+}
